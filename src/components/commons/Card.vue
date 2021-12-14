2914ade75fdeb198e69da5699c1c364541ef94c4
Marco Cusenza <marco.cusenza.me@gmail.com>
2021-12-14 19:23:54 +0100

    it works

--- src/components/commons/Card.vue
+++ src/components/commons/Card.vue
@@ -1,12 +1,34 @@
 <template>
-  <div class="card">aaa</div>
+  <div class="card">
+    <img :src="obj.thumb" :alt="obj.series" />
+    <div class="text">{{ obj.series }}</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Card",
+  props: {
+    obj: Object,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../assets/style/partials/variables.scss";
+
+.card {
+  width: 170px;
+  margin: 20px 20px;
+  text-align: left;
+
+  img {
+    height: 250px;
+  }
+  .text {
+    color: $light-text;
+    font-size: 13px;
+    text-transform: uppercase;
+  }
+}
 </style>
